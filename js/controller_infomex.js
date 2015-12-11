89285247cb904e3884b5b9199ff4a98375d58027
el coruco <el.coruco@gmail.com>
2015-12-10 22:44:04 -0600

    the heatmap

--- js/controller_infomex.js
+++ js/controller_infomex.js
@@ -14,7 +14,6 @@ define(function(require){
   var Backbone  = require('backbone'),
       d3        = require("d3"),
       HeatMap   = require("common_views/heat_map_view");
-
   //
   // C A C H E   T H E   C O M M O N   E L E M E N T S
   // --------------------------------------------------------------------------------
