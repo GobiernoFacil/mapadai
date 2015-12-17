282da3255d4d864ffea5f4c9abcb4c9e066f5cf3
el coruco <el.coruco@gmail.com>
2015-12-17 15:50:50 -0600

    timeline view

--- js/common_views/timelines_view.js
+++ js/common_views/timelines_view.js
@@ -1,7 +1,7 @@
 // INAI - Diagnósitco MapaDAImx
 // @package  : INAI
 // @location : /js/common_views
-// @file     : timelines_view.js
+// @file     : top10chart_view.js
 // @author   : Gobierno fácil <howdy@gobiernofacil.com>
 // @url      : http://gobiernofacil.com
 
@@ -14,11 +14,10 @@ define(function(require){
   var Backbone = require('backbone'),
       d3       = require("d3"),
       SVG      = require("common_views/main_svg_view"),
-      //Scales   = require("common_views/heat_map_scales_view"),
+      //Scale    = require("common_views/linear_scale_view"),
       //Axis     = require("common_views/heat_map_axis_view"), 
       //Color_r  = ["white", "green"],
-      Dummy    = [{"dependencia":"INSTITUTO MEXICANO DEL SEGURO SOCIAL","total":"25375"},{"dependencia":"SECRETARÃA DE EDUCACIÃ“N PÃšBLICA","total":"6238"},{"dependencia":"INSTITUTO DE SEGURIDAD Y SERVICIOS SOCIALES DE LOS TRABAJADORES DEL ESTADO","total":"5766"},{"dependencia":"SECRETARÃA DE SALUD","total":"4806"},{"dependencia":"PROCURADURÃA GENERAL DE LA REPÃšBLICA","total":"3793"},{"dependencia":"SECRETARÃA DE GOBERNACIÃ“N (INCLUYE LA ENTONCES SECRETARÃA DE SEGURIDAD PÃšBLICA)","total":"3754"},{"dependencia":"SECRETARÃA DE MEDIO AMBIENTE Y RECURSOS NATURALES","total":"3491"},{"dependencia":"SECRETARÃA DE COMUNICACIONES Y TRANSPORTES","total":"3123"},{"dependencia":"COMISIÃ“N FEDERAL PARA LA PROTECCIÃ“N CONTRA RIESGOS SANITARIOS","total":"2949"},{"dependencia":"PETRÃ“LEOS MEXICANOS","total":"2755"}];
-
+      URL      = "/js/data/timeline.json",
   //
   // D E F I N E   C O N S T A N T 'S
   // --------------------------------------------------------------------------------
@@ -28,7 +27,7 @@ define(function(require){
     height   : 600,
     top      : 20,
     right    : 30,
-    bottom   : 100, 
+    bottom   : 40, 
     left     : 30,
     padding  : 0,
     oPadding : 15
@@ -56,44 +55,50 @@ define(function(require){
     //
     // [ DEFINE THE TEMPLATES ]
     //
+    bar : _.template(Bar),
 
     //
     // [ THE INITIALIZE FUNCTION ]
     //
     //
     initialize : function(){
-      this.svg    = new SVG(this.el, Margins);
-      this.scales = new Scales(Margins);
-      this.axis   = new Axis(this.svg, this.scales, Margins);
-
+      //this.svg     = new SVG(this.el, Margins);
+      this.x_scale = new Scale(Dummy, Margins, "total", "x");
       this.render();
+      //this.scales = new Scales(Margins);
+      //this.axis   = new Axis(this.svg, this.scales, Margins);
+
+      //this.render();
     },
 
     render : function(){
-      var count  = 0,
-          days   = this.scales[0].domain(),
-          hours  = this.scales[1].domain(),
-          max    = d3.max(Dummy, function(n){
-            return n.count;
-          }),
-          color  = d3.scale.linear().domain([0, max]).range(Color_r); 
-
-      days.forEach(function(day, i){
-        hours.forEach(function(hour, j){
-          var datum  = Dummy[count],//Dummy[i + (j*7)],
-              x      = this.scales[0](day),
-              y      = this.scales[1](hour),
-              width  = this.scales[0].rangeBand(),
-              height = this.scales[1].rangeBand();
-          this.svg.append("rect")
-            .attr("x", x)
-            .attr("y", y)
-            .attr("width", width)
-            .attr("height", height)
-            .attr("fill", color(datum.count));
-          count++;
-        }, this);
+      /*
+      Dummy.forEach(function(d){
+        this.$el.append(this.bar(d));
       }, this);
+      */
+      var that = this,
+          divs = d3.select(this.el).selectAll("div")
+        .data(Dummy)
+        .enter()
+        .append("div")
+        .attr("class", "container");
+
+      divs.append("p")
+      .html(function(d){
+        return d.dependencia;
+      });
+
+      divs.append("div")
+        .attr("class", "bar")
+        .style({
+            background : "grey",
+            height : "30px",
+            width : function(d){
+              console.log(d, that.x_scale(d.total));
+              return that.x_scale(d.total) + "px";
+            }
+        });
     }
 
   });
