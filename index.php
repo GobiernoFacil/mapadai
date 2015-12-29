68f7f6b19354012fa68c397f2e19a1a8f4b91ef7
hugovom <hugo.om@gmail.com>
2015-12-29 12:45:22 -0600

    update index

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include "templates/header.php";?>
+    
+    <main class="main-base" role="main">
+	<header>
+		<div class="col-sm-6">
+			<h1 class="inai">INAI.  Instituto Nacional de Transparencia Acceso a la Información y Protección de Datos Personales </h1>
+		</div>
+		<div  class="clearfix"></div>
+	</header>
+	<!-- intro-->
+	<section class="i-intro">
+		<div class="years_title">
+			<h1>#MapaDAImx</h1>		
+			<div class="years"><span class="count">2014</span></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+					<p>Conoce la historia del derecho al acceso a la información en México, visualiza por año todos los datos del INAI.</p>
+					<a href="#" class="btn">NAVEGAR DATOS</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<ul class="year_nav">
+		<li><a href="#">2003</a></li>
+		<li><a href="#">2004</a></li>
+		<li><a href="#">2005</a></li>
+		<li><a href="#">2006</a></li>
+		<li><a href="#">2007</a></li>
+		<li><a href="#">2008</a></li>
+		<li><a href="#">2009</a></li>
+		<li><a href="#">2010</a></li>
+		<li><a href="#">2011</a></li>
+		<li><a href="#">2012</a></li>
+		<li><a href="#">2013</a></li>
+		<li><a href="#">2014</a></li>
+		<li><a href="#">2015</a></li>
+		<li><a href="#">Comparar Todos</a></li>
+	</ul>
+	
+	<div class="clearfix"></div>
+	
+	<!--CTA-->
+	<a href="#" class="btn proceso">CONOCE EL PROCESO DE SOLICITUD DE INFORMACIÓN</a>
+	
+<?php include "templates/footer.php";?>
