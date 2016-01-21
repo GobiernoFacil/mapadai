ad13f6a9545cf5b730b6196367b3f6868124148f
hugovom <hugo.om@gmail.com>
2016-01-20 20:24:45 -0600

    add link #13

--- infomex.php
+++ infomex.php
@@ -67,6 +67,7 @@
 					<h3>Top 10 de sujetos obligados por número de solicitudes</h3>
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
+							<a href="#" class="download" download>Descargar datos</a>
 							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>							
 						</div>
 					</div>
@@ -76,8 +77,10 @@
 				<!--heatmap-->
 				<div id="heatmap" class="viz">
 					<h3>Peticiones por hora por día de la semana</h3>
+					
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
+							<a href="#" class="download" download>Descargar datos</a>
 							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>							
 						</div>
 						<div class="col-sm-1 col-sm-offset-2">
