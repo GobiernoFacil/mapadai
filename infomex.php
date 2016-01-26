84677039c110504ae0343a9039524eaece4657c4
hugovom <hugo.om@gmail.com>
2016-01-25 18:18:24 -0600

    fix #13

--- infomex.php
+++ infomex.php
@@ -51,7 +51,7 @@
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
 							<h3>Top 10 de sujetos obligados por número de solicitudes: <span>2012 - 2015</span></h3>
-							<a href="#" class="download" download>Descargar datos</a>
+							<a href="#" class="download" download><b></b>Descargar datos</a>
 							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>							
 						</div>
 					</div>
