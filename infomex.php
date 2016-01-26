c2337074d8ff2cf0a55674923921f18008eba193
hugovom <hugo.om@gmail.com>
2016-01-25 19:41:15 -0600

    update responsive content

--- infomex.php
+++ infomex.php
@@ -32,7 +32,7 @@
 <div class="selector_year">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-10 col-sm-offset-1">
+			<div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1">
 				<div id="slider"></div>
 			</div>
 		</div>
