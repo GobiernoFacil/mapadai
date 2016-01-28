08f71a365462b8519fb1b9781837d3ac9586893f
hugovom <hugo.om@gmail.com>
2016-01-27 20:35:54 -0600

    update treemap @ infomex

--- infomex.php
+++ infomex.php
@@ -85,10 +85,10 @@
 				<!--treemap-->
 				<div id="treemap" class="viz">
 					<div class="row">
-						<div class="col-sm-10 col-sm-offset-1">
+						<div class="col-sm-12">
 							<h3>Top 10 de sujetos obligados por número de solicitudes: <span>2012 - 2015</span></h3>
 							<a href="#" class="download" download><b></b>Descargar datos</a>
-							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>							
+							<!--<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
 						</div>
 					</div>
 					<section id="treemap-a"></section>
