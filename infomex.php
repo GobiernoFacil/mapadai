9d3c66ed52feeeed685914040630828b59750d52
hugovom <hugo.om@gmail.com>
2016-02-03 02:00:24 -0600

    hide download btn

--- infomex.php
+++ infomex.php
@@ -56,9 +56,9 @@
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
 							<h3>Histórico de peticiones de información en las 10 principales dependencias: <span class="year-range">2013 - 2015</span></h3>
-												<a href="#" class="download" download><b></b>Descargar datos</a>
+							<!--						<a href="#" class="download" download><b></b>Descargar datos</a>
 
-						<!--	<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
+						<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
 						</div>
 					</div>
 					<section id="timeline-a"></section>
@@ -95,8 +95,8 @@
 					<div class="row">
 						<div class="col-sm-12">
 							<h3>Top 10 de sujetos obligados por número de solicitudes: <span class="year-range">2012 - 2015</span></h3>
-							<a href="#" class="download" download><b></b>Descargar datos</a>
-							<!--<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
+							<!--<a href="#" class="download" download><b></b>Descargar datos</a>
+							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
 						</div>
 					</div>
 					<section id="treemap-a"></section>
@@ -107,8 +107,8 @@
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
 							<h3>Peticiones por hora por día de la semana: <span class="year-range">2012 - 2015</span></h3>
-							<a href="#" class="download" download><b></b>Descargar datos</a>
-							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>							
+							<!--<a href="#" class="download" download><b></b>Descargar datos</a>
+							<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>		-->					
 						</div>
 						
 					</div>
