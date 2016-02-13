4aa7b5f83577ac6082e491f44b5aee71b1858561
hugovom <hugo.om@gmail.com>
2016-02-13 16:58:01 -0600

    add includes files

---
+++ includes/infomex/time.php
@@ -0,0 +1,18 @@
+<div id="time" class="viz">
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h3>Histórico de peticiones de información en las 10 principales dependencias: <span class="year-range">2013 - 2015</span></h3>
+			<!--						<a href="#" class="download" download><b></b>Descargar datos</a>
+
+		<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
+		</div>
+	</div>
+	<section id="timeline-a"></section>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<p class="source">
+				Fuente: INAI 2016
+			</p>
+		</div>
+	</div>
+</div>
