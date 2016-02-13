d86088387fe384c2bb7652758905bcb29109ea99
hugovom <hugo.om@gmail.com>
2016-02-13 17:19:51 -0600

    update top 10 #26

--- includes/infomex/time.php
+++ includes/infomex/time.php
@@ -1,10 +1,10 @@
 <div id="time" class="viz">
 	<div class="row">
-		<div class="col-sm-10 col-sm-offset-1">
-			<h3>Histórico de peticiones de información en las 10 principales dependencias: <span class="year-range">2013 - 2015</span></h3>
-			<!--						<a href="#" class="download" download><b></b>Descargar datos</a>
-
-		<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>	-->						
+		<div class="col-sm-12">
+			<h3>Top 10 histórico de sujetos obligados por solicitudes de acceso a la información y datos personales: 
+				<span class="year-range">2013 - 2015</span></h3>
+			<!--	<a href="#" class="download" download><b></b>Descargar datos</a>-->	
+			<p class="lead">Solicitudes que se realizan por sujeto obligado</p>					
 		</div>
 	</div>
 	<section id="timeline-a"></section>
