18736b262fc2e61f8949ec8d9ad093250057eb41
hugovom <hugo.om@gmail.com>
2016-02-13 18:10:54 -0600

    add bar files

---
+++ includes/infomex/bar.php
@@ -0,0 +1,17 @@
+<div id="top" class="viz">
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h3>Top 10 de sujetos obligados por solicitudes de acceso a la información y datos personales <span class="year-range">2013 - 2015</span></h3>
+			<!--<a href="#" class="download" download><b></b>Descargar datos</a>
+			<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>			-->				
+		</div>
+	</div>
+	<section id="top10bar"></section>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<p class="source">
+				Fuente: INAI 2016
+			</p>
+		</div>
+	</div>
+</div>
