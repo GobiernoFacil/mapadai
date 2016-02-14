bcc8eef538f3709f147d7983efaa2deb06816ff0
hugovom <hugo.om@gmail.com>
2016-02-14 15:59:51 -0600

    add pot files #28

---
+++ includes/pot/sessions/day.php
@@ -0,0 +1,33 @@
+<div id="heatmap" class="viz">
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h3>Peticiones por hora por día de la semana: <span class="year-range">2012 - 2015</span></h3>
+			<!--<a href="#" class="download" download><b></b>Descargar datos</a>
+			<p>Integer lectus purus, efficitur efficitur massa ut, pharetra cursus dolor. In hac habitasse platea dictumst. Vivamus quis neque ut neque eleifend volutpat. Curabitur lorem mauris, sagittis maximus ultrices ac, aliquam non ex. Donec congue maximus justo, sit amet euismod leo venenatis ac. Vivamus venenatis, risus vitae rutrum laoreet, purus nibh rutrum ex, sed convallis enim urna a ligula. Donec lorem leo, vulputate vel porttitor et, ornare in sapien.</p>		-->					
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-1 col-sm-offset-2">
+			<div class="legend">
+				<p>Horas</p>
+			</div>
+		</div>
+		<div class="col-sm-7">
+			<ul class="legend right">
+				<li class="zero"><b></b> <span>0</span></li>
+				<li class="fifty"><b></b> <span>50</span></li>
+				<li class="max"><b></b> <span>100</span></li>
+			</ul>
+		</div>
+		
+	</div>
+	<section id="heatmap-a"></section>
+	<!--source-->
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<!--source-->
+			<?php include "templates/source.php";?>
+		</div>
+	</div>
+</div>
---
+++ includes/pot/sessions/number.php
@@ -0,0 +1,16 @@
+<div id="time" class="viz">
+	<div class="row">
+		<div class="col-sm-12">
+			<h3>Top 10 histórico de sujetos obligados por solicitudes de acceso a la información y datos personales: 
+				<span class="year-range">2013 - 2015</span></h3>
+			<!--	<a href="#" class="download" download><b></b>Descargar datos</a>-->	
+			<p class="lead">Solicitudes que se realizan por sujeto obligado</p>					
+		</div>
+	</div>
+	<section id="timeline-a"></section>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<?php include "templates/source.php";?>
+		</div>
+	</div>
+</div>
