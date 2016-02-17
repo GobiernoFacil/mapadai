a6f549f9afbf938f798357e281a21c7f2963eb65
hugovom <hugo.om@gmail.com>
2016-02-17 04:49:15 -0600

    add pot-historico #28

---
+++ includes/pot/request/historico.php
@@ -0,0 +1,22 @@
+<div id="pot_historico" class="viz hide" data-viz="timeline-b">
+	<div class="row">
+		<div class="col-sm-12">
+			<h3>Top 10 de sujetos obligados por consultas en el POT</span>: <span class="year-range">2012 - 2015</span></h3>
+				<p class="lead">Posición de las distintas instituciones públicas en términos de número de consultas a lo largo del tiempo</p>
+			<!--<a href="#" class="download" download><b></b>Descargar datos</a>-->	
+		</div>
+		<div class="col-sm-10 col-sm-offset-1">
+			<p class="instructions">Da clic sobre un sector para observar la distribución del número de solicitudes por sujeto obligado 
+				y tipo de solicitud. Para regresar, da clic sobre el área gris en la parte superior de la gráfica.
+			</p>						
+		</div>
+	</div>
+	<section id="timeline-b"></section>
+	<!--source-->
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<!--source-->
+			<?php include "templates/source.php";?>
+		</div>
+	</div>
+</div>
