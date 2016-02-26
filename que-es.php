770614c8642931052c970cb7ac4c036d38a0328c
hugovom <hugo.om@gmail.com>
2016-02-25 21:47:49 -0600

    fix #35

--- que-es.php
+++ que-es.php
@@ -20,27 +20,42 @@
 					<div class="row">
 						<div class="col-sm-8 col-sm-offset-2">
 							<h3>#MapaDAImx</h3>
-							<p>El proyecto estratégico Herramienta #MapaDAImx se propone como un instrumento para visualizar el diagnóstico coordinado por la <strong>Dirección General de Políticas de Acceso (DGPA)</strong> que a su vez, busca homologar la información de carácter nacional generada por distintas bases de datos en las entidades federativas en un sólo cubo de información a nivel nacional.</p>
+							<p>El proyecto estratégico Herramienta <strong>#MapaDAImx</strong> se propone como un 
+								instrumento para visualizar el 
+								diagnóstico que se elaboró con el 
+								propósito de conocer la manera en 
+								que se ha ejercido el derecho de 
+								acceso a la información, a través del 
+								análisis de la información contenida 
+								en las herramientas informáticas que 
+								para su ejercicio y garantía se han 
+								desarrollado por el  <a href="http://inicio.ifai.org.mx/">INAI</a>.</p>
 
 							<div class="divider"></div>
 							<h3>Objetivo</h3>
-							<p>Crear una herramienta tecnológica para visualizar el diagnóstico <strong>#MapaDAImx</strong>, y que en el mediano plazo pueda llegar a integrar la información de los distintos sistemas informáticos y bases de datos del <a href="http://inicio.ifai.org.mx/">INAI</a>, al igual que de todos los demás organismos garantes a nivel nacional, que permita el análisis sobre el ejercicio y garantía del derecho de acceso a la información en México y; asimismo, posibilite diseñar políticas basadas en evidencia.</p>
+							<p>Crear una herramienta tecnológica
+								para visualizar el diagnóstico 
+								<strong>#MapaDAImx</strong>, y que permite realizar 
+								inferencia estadística acerca del 
+								comportamiento de los usuarios del 
+								derecho de acceso a la información.</p>
 							
 							<div class="divider"></div>
 							<h3>Alcance</h3>
-							<p>La herramienta mostrará parte de la actividad del <a href="http://inicio.ifai.org.mx/">INAI</a>, presentando estadísticas de las <a href="/infomex.php">solicitudes de información</a>. Se programarán una serie de gráficas que muestran la actividad de estas solicitudes. La información se obtendrá de distintas APIs </p>
-							<!-- O_O
-							<p>Contará con las secciones de: </p>
-							<ul>
-								<li>Proceso de petición de información</li>
-								<li>POT								  </li>
-								<li>Infomex							  </li>
-								<li>Recursos de revisión			  </li>
-							</ul>
+							<p>La herramienta mostrará información sobre: </p>
+							<ol>
+								<li>Consultas al Portal de Obligaciones de Transparencia del Poder Ejecutivo Federal.</li>
+								<li>Solicitudes de acceso a información pública, de acceso a datos personales y de corrección de datos personales y sus respuestas en el ámbito de la Administración Pública Federal.</li>
+								<li>Recursos de revisión interpuestos contra respuestas a solicitudes de acceso a información pública, de datos personales y de corrección de datos personales.</li>
 
-							<p>En la primera sección, el proceso de petición de información, se programará un retícula que permita ir avanzando entre los distintos caminos que puede tomar el proceso de petición de información. Cada paso contendrá información relevante del proceso, y si es posible, información estadística sobre el mismo.</p>
 
-							<p>Para la sección del POT, de Infomex y Recursos de revisión, se programará una serie de gráficas que muestren la actividad de cada una de ellas. La información se obtendrá de distintas apis.</p> -->
+							</ol>
+
+							<p>Lo anterior, con base en lo dispuesto por la <strong>Ley Federal de Transparencia y Acceso a la Información Pública 
+								Federal</strong>, su Reglamento y demás disposiciones aplicables.</p>
+							<p>También se incluye un apartado respecto del año de 2014, con información de entidades federativas y de los otros 
+								sujetos obligados de la <strong>Ley de Transparencia y Acceso a la Información Pública Gubernamental</strong>. 
+								La información se obtendrá de distintas APIs.</p>
 						</div>
 					</div>
 					
