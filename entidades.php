73efb2881351a631ab5b88fc9acc621ac1ae749a
hugovom <hugo.om@gmail.com>
2016-03-02 15:52:32 -0600

    add link to files

--- entidades.php
+++ entidades.php
@@ -26,16 +26,16 @@
 							<p>Derivado de esta solicitud fue entregada la siguiente información por parte de las entidades federativas 
 								y los otros sujetos obligados:</p>
 							<div class="row table_header">
-								<div class="col-sm-4">
+								<div class="col-sm-6">
 									<p>Sujeto Obligado</p>
 								</div>
 								<div class="col-sm-2">
 									<p>Cuestionario</p>
 								</div>
-								<div class="col-sm-3">
+								<div class="col-sm-2">
 									<p>Base de datos solicitudes</p>
 								</div>
-								<div class="col-sm-3">
+								<div class="col-sm-2">
 									<p>Base de datos recursos</p>
 								</div>
 							</div>
@@ -47,407 +47,416 @@
 									</span>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!-- Baja Califormia-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Baja California</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!-- Baja California Sur-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Baja California Sur</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
 								</li>
 								<!--Campeche-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Campeche</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Chiapas-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Chiapas</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Chihuahua-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Chihuahua</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
 								</li>
 								<!--Coahuila-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Coahuila</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Colima-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Colima</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Distrito Federal-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Distrito Federal</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Durango-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Durango</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Estado de México-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Estado de México</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Guanajuato-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Guanajuato</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Guerrero-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Guerrero</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Hidalgo-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Hidalgo</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Jalisco-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Jalisco</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Michoacán-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Michoacán</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
 								</li>
 								<!--Morelos-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Morelos</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Nayarit-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Nayarit</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Nuevo León-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Nuevo León</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Oaxaca-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Oaxaca</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Puebla-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Puebla</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
 								</li>
 								<!--Querétaro-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Querétaro</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Quintana Roo-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Quintana Roo</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--San Luis Potosí-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>San Luis Potosí</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Sinaloa-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Sinaloa</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Sonora-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Sonora</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Tabasco-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Tabasco</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Tamaulipas-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Tamaulipas</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Tlaxcala-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Tlaxcala</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Veracruz-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Veracruz</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
 								</li>
 								<!--Yucatán-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Yucatán</p>
 									</div>
 									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 								<!--Zacatecas-->
 								<li class="row">
-									<div class="col-sm-4">
+									<div class="col-sm-6">
 										<p>Zacatecas</p>
 									</div>
 									<div class="col-sm-2">
 										<div class="status_ok"></div>
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
-									<div class="col-sm-3">
+									<div class="col-sm-2">
 									</div>
 								</li>
 							</ol>
 							
 							<p>Los motivos por los cuales no fue entregada la información solicitada por algunas entidades federativas así como por los otros sujetos obligados, fueron por problemas diversos, muchos relacionados con capacidades tecnológicas.</p>
+
 							<div class="divider"></div>
+							<h3>Cuestionarios</h3>
+							<p>
+							<a href="/files/DGPA_MapaDAI_edos_conINFOMEX.pdf" class="btn file">Cuestionario para Órganos Garantes de las entidades federativas con INFOMEX</a></p>
+							<p>
+							<a href="/files/DGPA_MapaDAI_edos_sinINFOMEX.pdf" class="btn file">Cuestionario para Órganos Garantes de las entidades federativas sin INFOMEX</a></p>
+							
+							<div class="divider"></div>
+
 						</div>
 					</div>
 					
