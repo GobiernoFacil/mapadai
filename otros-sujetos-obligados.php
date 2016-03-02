f89da4754308742429f732cdc5b77f544c6e6d0d
hugovom <hugo.om@gmail.com>
2016-03-02 15:49:18 -0600

    fix #47

---
+++ otros-sujetos-obligados.php
@@ -0,0 +1,327 @@
+<?php 
+	$body_class 	= "otros";
+	$title 			= "Otros sujetos obligados | INAI";
+	$description 	= "Para el desarrollo del Diagnóstico #MapaDAImx, el INAI solicitó a los otros sujetos obligados en el ámbito federal, su colaboración";
+	include "templates/header.php";?>
+<div class="sub">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Otros sujetos obligados</h1>
+			</div>
+		</div>
+	</div>
+</div>
+ <div class="content_graph">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="viz">
+					<div class="row">
+						<div class="col-sm-8 col-sm-offset-2">
+							<p>Para el desarrollo del Diagnóstico #MapaDAImx, el INAI solicitó a los otros sujetos obligados en el ámbito federal, su colaboración para atender por una parte, un cuestionario sobre solicitudes de información y recursos de revisión recibidos en su ámbito de competencia y, en su caso, sobre información del uso del Portal de Obligaciones de Transparencia en el cual se publican las obligaciones de transparencia establecidas por la Ley Federal de Transparencia y Acceso a la Información Pública Gubernamental; y, por la otra, las consultas a bases de datos en los casos que los órganos garantes de los sujetos obligados contaran con Infomex, para extraer información sobre solicitudes de acceso y recursos de revisión recibidos en su ámbito de competencia.</p>
+
+							<p>Los cuestionarios sobre solicitudes de acceso y recursos de revisión fueron distintos, en atención a la existencia del Sistema Infomex en las entidades federativas.</p>
+							<p>DDerivado de esta solicitud fue entregada la siguiente información por parte de los otros sujetos obligados:</p>
+							<div class="row table_header">
+								<div class="col-sm-6">
+									<p>Sujeto Obligado</p>
+								</div>
+								<div class="col-sm-2">
+									<p>Cuestionario</p>
+								</div>
+								<div class="col-sm-2">
+									<p>Base de datos solicitudes</p>
+								</div>
+								<div class="col-sm-2">
+									<p>Base de datos recursos</p>
+								</div>
+							</div>
+							<ol class="table_entidades">
+								<!-- Tribunal Federal de Conciliación y Arbitraje-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Tribunal Federal de Conciliación y Arbitraje</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Universidad Autónoma Metropolitana-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Universidad Autónoma Metropolitana</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Auditoria Superior de la Federación-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Auditoria Superior de la Federación</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Suprema Corte de Justicia de la Nación-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Suprema Corte de Justicia de la Nación</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Banco de México-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Banco de México</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Comisión Nacional de los Derechos Humanos-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Comisión Nacional de los Derechos Humanos</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Instituto Nacional de Estadística y Geografía-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Instituto Nacional de Estadística y Geografía</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Instituto Nacional para la Evaluación de la Educación-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Instituto Nacional para la Evaluación de la Educación</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Comisión Federal de Competencia Económica-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Comisión Federal de Competencia Económica</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Instituto Federal de Telecomunicaciones-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Instituto Federal de Telecomunicaciones</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Cámara de senadores-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Cámara de senadores</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Tribunal Federal de Justicia Fiscal y Administrativa-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Tribunal Federal de Justicia Fiscal y Administrativa</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Tribunal Superior Agrario-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Tribunal Superior Agrario</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Cámara de Diputados-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Cámara de Diputados</p>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Tribunal Electoral del Poder Judicial de la Federación-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Tribunal Electoral del Poder Judicial de la Federación</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+								<!-- Consejo de la Judicatura Federal-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Consejo de la Judicatura Federal</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Universidad Nacional Autónoma de México-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Universidad Nacional Autónoma de México</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!--Universidad Autónoma de Chapingo-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Universidad Autónoma de Chapingo</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+									</div>
+									<div class="col-sm-2">
+									</div>
+								</li>
+								<!-- Instituto Nacional Electoral-->
+								<li class="row">
+									<div class="col-sm-6">
+										<p>Instituto Nacional Electoral</p>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+									<div class="col-sm-2">
+										<div class="status_ok"></div>
+									</div>
+								</li>
+							</ol>
+							
+							<p>Los motivos por los cuales no fue entregada la información solicitada por algunas entidades federativas así como por los otros sujetos obligados, fueron por problemas diversos, muchos relacionados con capacidades tecnológicas.</p>
+							
+							<div class="divider"></div>
+							<h3>Cuestionarios</h3>
+							<p>
+							<a href="/files/DGPA_MapaDAI_osos_conINFOMEX.pdf" class="btn file">Cuestionario para Órganos Garantes de los Otros Sujetos Obligados de la Ley Federal de Transparencias y Acceso a la Información Pública Gubernamental con INFOMEX</a></p>
+							<p>
+							<a href="/files/DGPA_MapaDAI_osos_sinINFOMEX.pdf" class="btn file">Cuestionario para Órganos Garantes de los Otros Sujetos Obligados de la Ley Federal de Transparencias y Acceso a la Información Pública Gubernamental sin INFOMEX</a></p>
+							
+							<div class="divider"></div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+ </div>
+ 
+  <script>
+  CONFIG = { section : "que-es" };
+  
+</script>
+<?php include "templates/footer.php";?>
