23cd93c8250d4bf012f405ba1085d822a77f3337
hugovom <hugo.om@gmail.com>
2016-12-18 13:15:20 -0600

    update title

--- includes/recursos/obligee/total.php
+++ includes/recursos/obligee/total.php
@@ -1,7 +1,7 @@
 <div id="obligee_total" class="viz hide" data-viz="bar">
 	<div class="row">
 		<div class="col-sm-10 col-sm-offset-1">
-			<h3>Top 10 de sujetos obligados por recursos de revisión y sentido de la resolución <span class="year-range">2013 - 2015</span></h3>
+			<h3>Top 10 de sujetos obligados por recursos de revisión (total): <span class="year-range">2013 - 2015</span></h3>
 			<!--<a href="#" class="download" download><b></b>Descargar datos</a>-->			
 			<p class="lead">Total de recursos de revisión por sujeto obligado</p>	
 		</div>
