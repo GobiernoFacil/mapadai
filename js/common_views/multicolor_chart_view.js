87776db48e361217ef3fdd9a92bb59a3e2c43fd9
elcoruco <el.coruco@gmail.com>
2016-12-27 23:22:56 -0600

    add multicolor layer

--- js/common_views/multicolor_chart_view.js
+++ js/common_views/multicolor_chart_view.js
@@ -34,7 +34,7 @@ define(function(require){
   Rect = {
     height : 10,
     fill   : "#3498DB",
-    colors : ["#242B40","#1AB6D9", "#1AC6D9", "#D9A74A", "#BF612A",  "#DF3190", "#A69203","#10E684", "#F4A775"],
+    colors : ["#4d7390","#5dabdd", "#42a7b0", "#f26244", "#BF612A",  "#DF3190", "#A69203","#10E684", "#F4A775"],
     fillw  : "#981F7C",
     slot   : 30, // el espacio entre rectángulo y rectángulo
     empty  : "sin especificar" 
@@ -91,7 +91,7 @@ define(function(require){
         .append("g")
         .attr("class", "multicolor-ticks")
         .attr("transform", function(d){
-          return "translate(" + that.scale(d) + ", 0)";
+          return "translate(" + (that.scale(d) + Margins.left) + ", 0)";
         })
           .append("line")
             .attr("x0", 0)
@@ -151,6 +151,94 @@ define(function(require){
 
       this.bars.exit().remove();
 
+      /********/
+      this.barsA = this.svg.selectAll(".A-rect-media").data(this.data);
+
+      this.barsA.attr("width", function(d){
+               return that.scale(d.totalA);
+          });
+
+      this.barsA.enter()
+        .append("rect")
+          .attr("class", "A-rect-media")
+          .attr("fill", Rect.colors[0])
+          //.attr("fill-opacity", "0.5")
+          .attr("width", function(d){
+               return that.scale(d.totalA);
+          })
+          .attr("height", Rect.height)
+          .attr("x", Margins.left)
+          .attr("y", function(d, i){
+            return (Rect.slot * i) + Margins.top + 5;
+          });
+
+         
+
+      this.barsA.exit().remove();
+
+
+      this.barsB = this.svg.selectAll(".B-rect-media").data(this.data);
+
+      this.barsB.attr("width", function(d){
+               return that.scale(d.totalB);
+          }).attr("x", function(d){
+            return Margins.left + that.scale(d.totalA)
+          });
+
+      this.barsB.enter()
+        .append("rect")
+          .attr("class", "B-rect-media")
+          .attr("fill", Rect.colors[1])
+          //.attr("fill-opacity", "0.5")
+          .attr("width", function(d){
+               return that.scale(d.totalB);
+          })
+          .attr("height", Rect.height)
+          .attr("x", function(d){
+            return Margins.left + that.scale(d.totalA)
+          })
+          .attr("y", function(d, i){
+            return (Rect.slot * i) + Margins.top + 5;
+          });
+
+         
+
+      this.barsB.exit().remove();
+
+
+
+      this.barsC = this.svg.selectAll(".C-rect-media").data(this.data);
+
+      this.barsC.attr("width", function(d){
+               return that.scale(d.totalC);
+          }).attr("x", function(d){
+            return Margins.left + that.scale(d.totalA) + that.scale(d.totalB)
+          });
+
+      this.barsC.enter()
+        .append("rect")
+          .attr("class", "C-rect-media")
+          .attr("fill", Rect.colors[2])
+          //.attr("fill-opacity", "0.5")
+          .attr("width", function(d){
+               return that.scale(d.totalC);
+          })
+          .attr("height", Rect.height)
+          .attr("x", function(d){
+            return Margins.left + that.scale(d.totalA) + that.scale(d.totalB)
+          })
+          .attr("y", function(d, i){
+            return (Rect.slot * i) + Margins.top + 5;
+          });
+
+         
+
+      this.barsC.exit().remove();
+
+
+      /****/
+
+
       this.multicolor_labels = this.svg.selectAll(".multicolor-label").data(this.data);
 
       this.multicolor_labels.enter()
@@ -176,7 +264,7 @@ define(function(require){
     },
 
     _scale : function(data){
-      var x       = [Margins.left, Margins.width - Margins.left - Margins.right],
+      var x       = [0, Margins.width - Margins.left - Margins.right],
           x_array = _.pluck(data, "total"),
           extent  = d3.extent(x_array),
           scale   = d3.scale.linear()
@@ -206,7 +294,8 @@ define(function(require){
     },
 
     map_data : function(){
-      var solicitudes   = _.uniq(_.pluck(this.__data, "tiposolicitud")),
+      var that          = this,
+          solicitudes   = _.uniq(_.pluck(this.__data, "tiposolicitud")),
           _dependencias = _.uniq(_.pluck(this.__data, "dependencia")),
           dependencias  = _dependencias.map(function(d){
             var r = {
@@ -220,20 +309,31 @@ define(function(require){
                         return a + b;
                       }, 0);
 
-            /*
-            solicitudes.forEach(function(s){
-              r[s] = _.where(r.data, {tiposolicitud : s});
-            }, this);
-            */
+            r.totalA = _.pluck(r.data.filter(function(s){
+                             return s.tiposolicitud == solicitudes[0];
+                           }), "total").reduce(function(a, b){
+                             return Number(a) + Number(b);
+                           }, 0);
+
+            r.totalB = _.pluck(r.data.filter(function(s){
+                             return s.tiposolicitud == solicitudes[1];
+                           }), "total").reduce(function(a, b){
+                             return Number(a) + Number(b);
+                           }, 0);
+
+            r.totalC = _.pluck(r.data.filter(function(s){
+                             return s.tiposolicitud == solicitudes[2];
+                           }), "total").reduce(function(a, b){
+                             return Number(a) + Number(b);
+                           }, 0);
+
 
             return r;
           }, this);
 
       dependencias = _.sortBy(dependencias, function(s){return -s.total});
 
-      console.log(_.pluck(dependencias, "total"));
-
-      this.data    = dependencias;
+      this.data    = dependencias;//.slice(0, 100);
       this.options = solicitudes;
     },
   });
