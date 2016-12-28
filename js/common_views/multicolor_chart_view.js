c43e7bf24abc7ea3702505c6742c40d7d3d6246b
elcoruco <el.coruco@gmail.com>
2016-12-27 21:05:34 -0600

    sepa qué sea este cambio :(

--- js/common_views/multicolor_chart_view.js
+++ js/common_views/multicolor_chart_view.js
@@ -32,7 +32,7 @@ define(function(require){
     oPadding : 15
   },
   Rect = {
-    height : 5,
+    height : 10,
     fill   : "#3498DB",
     colors : ["#242B40","#1AB6D9", "#1AC6D9", "#D9A74A", "#BF612A",  "#DF3190", "#A69203","#10E684", "#F4A775"],
     fillw  : "#981F7C",
@@ -76,9 +76,9 @@ define(function(require){
       Current_range = range;
       this.__data = data;
       this.map_data();
-      this.scale = this._scale(this.data);;
+      this.scale = this._scale(this.data);
       
-      Margins.height = (data.length * Rect.slot) + Margins.top + Margins.bottom;
+      Margins.height = (this.data.length * Rect.slot) + Margins.top + Margins.bottom;
 
       if(!this.svg){
         this.svg = this.make_svg(Margins);
@@ -130,7 +130,7 @@ define(function(require){
           .attr("x", 0)
           .attr("y", Margins.height - Margins.bottom);
 
-      this.bars = this.svg.selectAll(".multicolor-rect-media").data(data);
+      this.bars = this.svg.selectAll(".multicolor-rect-media").data(this.data);
       this.bars.enter()
         .append("rect")
           .attr("class", "multicolor-rect-media")
@@ -144,97 +144,35 @@ define(function(require){
             return (Rect.slot * i) + Margins.top + 5;
           });
 
+      this.bars.attr("width", function(d){
+            return that.scale(d.total);
+          });
+         
+
       this.bars.exit().remove();
 
-      return;
-      /*
-      data = _.sortBy(data, function(d){return -d.suma;});
-      if(!this.svg){
-        this.svg = this.make_svg(Margins);
-      }
+      this.multicolor_labels = this.svg.selectAll(".multicolor-label").data(this.data);
 
-      var x_scale = this.scale(data);
-
-      // Las líneas verticales
-      this.svg.selectAll(".occupation-ticks").remove();
-      this.ticks = this.svg.selectAll(".occupation-ticks").data(x_scale.ticks());
-      this.ticks.enter()
-        .append("g")
-        .attr("class", "occupation-ticks")
-        .attr("transform", function(d){
-          return "translate(" + x_scale(d) + ", 0)";
-        })
-          .append("line")
-            .attr("x0", 0)
-            .attr("x1",0)
-            .attr("y0", 30)
-            .attr("y1", Margins.height - Margins.bottom )
-            .style({
-              "stroke" : "#ddd",
-              "stroke-width" : 1
-            });
-      this.ticks.exit().remove();
-
-      
-      // los números de las líneas verticales
-      this.ticks_top_labels = this.svg.selectAll(".occupation-ticks");
-      this.ticks_top_labels.selectAll(".occupation-tick-label-top").remove();
-      this.ticks_top_labels.append("text")
-          .attr("class", "occupation-tick-label-top")
-          .attr("fill", "black")
-          .attr("text-anchor", "middle")
-          .text(function(d){
-            return Format(d);
-          })
-          .attr("x", 0)
-          .attr("y", 10);
-
-      this.ticks_bottom_labels = this.svg.selectAll(".occupation-ticks");
-      this.ticks_bottom_labels.selectAll(".occupation-tick-label-bottom").remove();
-      
-      this.ticks_bottom_labels.append("text")
-          .attr("class", "occupation-tick-label-bottom")
-          .attr("fill", "black")
-          .attr("text-anchor", "middle")
-          .text(function(d){
-            return Format(d);
-          })
-          .attr("x", 0)
-          .attr("y", Margins.height - Margins.bottom);
-
-      
-
-      this.bars2 = this.svg.selectAll(".occupation-rect-women").data(data);
-      this.bars2.enter()
-        .append("rect")
-          .attr("class", "occupation-rect-women")
-          .attr("fill", Rect.fillw)
-          .attr("width", function(d){
-            return x_scale(+d.suma);
-          })
-          .attr("height", Rect.height)
-          .attr("x", Margins.left)
-          .attr("y", function(d, i){
-            return (Rect.slot * i) + Margins.top + 5;
-          });
-
-      this.bars2.exit().remove();
-*/
-      this.occupation_labels = this.svg.selectAll(".occupation-label").data(data);
-
-      this.occupation_labels.enter()
+      this.multicolor_labels.enter()
         .append("text")
-          .attr("class", "occupation-label")
+          .attr("class", "multicolor-label")
           .attr("fill", "black")
           .text(function(d){
-            return d.ocupacion.trim() ? d.ocupacion : Rect.empty;
+            return d.dependencia.trim() ? (d.dependencia + " : " + d.total) : Rect.empty;
           })
           .attr("x", Margins.left)
           .attr("y", function(d, i){
             return (Rect.slot * i) + Margins.top;
           });
 
-      this.occupation_labels.exit().remove();
+      this.multicolor_labels
+          .text(function(d){
+            return d.dependencia.trim() ? (d.dependencia + " : " + d.total) : Rect.empty;
+          });
+
+      this.multicolor_labels.exit().remove();
+
+      return;
     },
 
     _scale : function(data){
@@ -242,23 +180,10 @@ define(function(require){
           x_array = _.pluck(data, "total"),
           extent  = d3.extent(x_array),
           scale   = d3.scale.linear()
-                    .domain(extent)
+                    .domain([0, extent[1]])
                     .range(x);
 
           return scale;
-
-      // console.log(extent, x_array);
-      /*
-
-          x      = [Margins.left, Margins.width - Margins.left - Margins.right],
-          extent = d3.extent(data, function(d){
-            return d.suma;
-          }),
-          scale = d3.scale.linear()
-                    .domain(extent)
-                    .range(x);
-      return scale;
-      */
     },
 
     get_range : function(){
@@ -304,25 +229,12 @@ define(function(require){
             return r;
           }, this);
 
+      dependencias = _.sortBy(dependencias, function(s){return -s.total});
+
+      console.log(_.pluck(dependencias, "total"));
+
       this.data    = dependencias;
       this.options = solicitudes;
-
-      console.log(this.options, this.data);
-
-      /*
-      dependencia
-:
-"SECRETARÍA DE ENERGÍA"
-tiporespuesta
-:
-"Informacion disponible"
-tiposolicitud
-:
-"Datos Personales"
-total
-:
-"4"
-      */
     },
   });
 
